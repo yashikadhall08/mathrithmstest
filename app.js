32a6fe93a0a28d3a8c40c1067a3063b7d5a8514f
yashikadhall08 <52121607+yashikadhall08@users.noreply.github.com>
2021-09-14 12:29:03 +0530

    Create app.js
    Rest API

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const app = express();
+
+const userData = [
+  {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874"
+    }
+  },
+  {
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.tv",
+    "address": {
+      "street": "Victor Plains",
+      "suite": "Suite 879",
+      "city": "Wisokyburgh",
+      "zipcode": "90566-7771"
+    }
+  },
+  {
+    "id": 3,
+    "name": "Clementine Bauch",
+    "username": "Samantha",
+    "email": "Nathan@yesenia.net",
+    "address": {
+      "street": "Douglas Extension",
+      "suite": "Suite 847",
+      "city": "McKenziehaven",
+      "zipcode": "59590-4157"
+    }
+  },
+  {
+    "id": 4,
+    "name": "Patricia Lebsack",
+    "username": "Karianne",
+    "email": "Julianne.OConner@kory.org",
+    "address": {
+      "street": "Hoeger Mall",
+      "suite": "Apt. 692",
+      "city": "South Elvis",
+      "zipcode": "53919-4257"
+    }
+  },
+  {
+    "id": 5,
+    "name": "Chelsey Dietrich",
+    "username": "Kamren",
+    "email": "Lucio_Hettinger@annie.ca",
+    "address": {
+      "street": "Skiles Walks",
+      "suite": "Suite 351",
+      "city": "Roscoeview",
+      "zipcode": "33263"
+    }
+  },
+
+app.get("/users", (req,res) =>{
+   res.json({data : userData});
+})
+
+app.get("/users/:id" , (req,res) =>{
+  let id = req.params.id;
+  
+  let user = userData.filter((data) => {
+   if(data.id == id)
+    return true;
+  })
+  
+  res.json({data : user});
+  
+})
+
+app.listen(5001);
